c616bfa50932d283109808719660c1ca7cba3f2f
MSerega <95672409+MSerega@users.noreply.github.com>
2023-05-27 08:09:06 +0300

    Додана команда перезапуску бота на сервері

--- handlers/administrator/cmd_reboot_bot.py
+++ handlers/administrator/cmd_reboot_bot.py
@@ -1,3 +1,4 @@
+import asyncio
 from aiogram import types
 from loader import dp
 import subprocess
@@ -8,7 +9,9 @@ from settings.config import ADMIN
 @dp.message_handler(commands=['reboot_bot'], chat_type=types.ChatType.PRIVATE, chat_id=ADMIN, state="*")
 async def command_start(message: types.Message):
     try:
-        await message.answer(f"Перезавантаження бота", disable_notification=True)
+        await message.answer(f"Перезавантаження ботів", disable_notification=True)
         subprocess.call(["pm2", "restart", "0"])
+        await asyncio.sleep(3)
+        subprocess.call(["pm2", "restart", "1"])
     except subprocess.CalledProcessError:
         await message.answer(f"Сталась помилка перезавантаження", disable_notification=True)
