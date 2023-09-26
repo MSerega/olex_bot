7184c70fa9b19d9b8d5a90799c3ef05d2adf907e
Gurtovoy <mseregax100@ukr.net>
2023-09-26 08:30:50 +0300

    Вимкнений режим тиші в чаті

--- settings/on_clock.py
+++ settings/on_clock.py
@@ -7,11 +7,11 @@ from scripts.news_rss import generalised_parse
 
 async def scheduler():
     aioschedule.every().day.at("00:00").do(update_horoscope)
-    aioschedule.every().day.at("7:00").do(handlers.groups.change_permissions.message_permissions_access)
+    # aioschedule.every().day.at("7:00").do(handlers.groups.change_permissions.message_permissions_access)
     aioschedule.every().day.at("7:30").do(handlers.channels.morning_pidsluhaho.morning_weather)
     # aioschedule.every().day.at("8:30").do(handlers.channels.morning_pidsluhaho.morning_fuel)
     aioschedule.every().day.at("9:00").do(handlers.channels.morning_pidsluhaho.morning_memory)
-    aioschedule.every().day.at("23:00").do(handlers.groups.change_permissions.message_permissions_block)
+    # aioschedule.every().day.at("23:00").do(handlers.groups.change_permissions.message_permissions_block)
     # for hour in range(7, 24):
     #     aioschedule.every().day.at(f"{hour}:00").do(generalised_parse)
     while True:
